9ba80e3a6e221f919668f12f59f9b0a82990bf6e
Krisha Busa <23010101039@darshan.ac.in>
2024-06-21 23:46:52 +0530

    Add files via upload

---
+++ LAB_4/Lab_4A_2.c
@@ -0,0 +1,9 @@
+
+#include<stdio.h>
+void main(){
+	printf("Address :\n");
+	printf("Block no.\n");
+	printf("Shraddhadeep Park\n");
+	printf("80ft. Nehru Nagar Main Road,\n");
+	printf("Rajkot-360002.\n");
+}
---
+++ LAB_4/Lab_4A_3.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+void main(){
+	int a,b,c;
+	printf("Enter the value A\n");
+	scanf("%d",&a);
+	printf("Enter the value B\n");
+	scanf("%d",&b);
+	c=a+b;
+	printf("Value of c = %d",c);
+}
